a24dc4bd374b62fb9d62c51cf3c28a17923ffbfd
Josinaldo Júnior <josinaldo.junior@capgemini.com>
2020-02-04 19:45:13 -0300

    Estrutura inicial do projeto.

---
+++ backend/api_agenda/app/Http/Controllers/ModelsContactController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ApiAgenda\Http\Controllers;
+
+use ApiAgenda\ModelsContact;
+use Illuminate\Http\Request;
+
+class ModelsContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \ApiAgenda\ModelsContact  $modelsContact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModelsContact $modelsContact)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \ApiAgenda\ModelsContact  $modelsContact
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModelsContact $modelsContact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \ApiAgenda\ModelsContact  $modelsContact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModelsContact $modelsContact)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \ApiAgenda\ModelsContact  $modelsContact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModelsContact $modelsContact)
+    {
+        //
+    }
+}
