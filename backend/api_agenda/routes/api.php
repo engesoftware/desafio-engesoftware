214426b7055a0e4dc7b1a4770bf7569c398cd3db
Josinaldo Júnior <josinaldo.junior@capgemini.com>
2020-02-06 09:07:28 -0300

    Implementação da proteção da API

--- backend/api_agenda/routes/api.php
+++ backend/api_agenda/routes/api.php
@@ -19,6 +19,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['namespace' => 'Api', 'as' => 'api.'], function () {
     Route::name('login')->post('login', 'AuthController@login');
-    Route::resource('contacts', 'ContactController', ['except' => ['create', 'edit']]);
-    Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::resource('contacts', 'ContactController', ['except' => ['create', 'edit']]);
+        Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
+    });
 });
