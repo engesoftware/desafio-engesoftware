97dcd4a38b8ac3848171667f0b96251f068638ba
cesarionto <cesariopereiraneto@gmail.com>
2020-02-09 09:42:25 -0300

    configurei o banco de dados postgres, criei a classe para Contato e ContatoRepository

---
+++ desafioagendacesario/desafioagendabackend/src/main/java/com/engsoftware/desafioagendabackend/DesafioagendabackendApplication.java
@@ -0,0 +1,13 @@
+package com.engsoftware.desafioagendabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioagendabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioagendabackendApplication.class, args);
+	}
+
+}
---
+++ desafioagendacesario/desafioagendabackend/src/test/java/com/engsoftware/desafioagendabackend/DesafioagendabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engsoftware.desafioagendabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioagendabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
